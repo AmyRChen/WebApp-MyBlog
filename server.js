467089aa2917c241bb6754df5e7f65c75939b371
Ping-Ju, Chen <cpr860632231@gmail.com>
2022-09-27 15:49:46 -0400

    assignment2

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: PingJu Chen     Student ID: 151043205   Date: 2022/09/27
+*
+*  Online (Cyclic) Link: https://tense-pink-top-coat.cyclic.app
+*
+********************************************************************************/ 
+
+
 var express = require("express");
 const data = require("./blog-service.js");
 const path = require("path");
