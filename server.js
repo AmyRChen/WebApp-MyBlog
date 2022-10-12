de3d9b1c12cc53f30db25b57b29fe064fdcaf7fe
Ping-Ju, Chen <cpr860632231@gmail.com>
2022-10-12 11:31:38 -0400

    adjustment for add function

--- server.js
+++ server.js
@@ -57,15 +57,14 @@ app.get("/posts", function(req, res){
         data.getPostsByCategory(req.query.category).then((data)=>{
             res.json(data);
         }).catch((err)=>{
-            res.send("Message: Something went wrong, " + err);  //need?
+            res.send("Message: Something went wrong, " + err);
         })
     }  
     else if(req.query.minDate){
         data.getPostsByMinDate(req.query.minDate).then((data) =>{
             res.json(data);
-            //res.json({message: req.query.minDate});
         }).catch((err)=>{
-            res.send("Message: Something went wrong, " + err);  //need?
+            res.send("Message: Something went wrong, " + err);
         })
     }
     else{
@@ -90,34 +89,47 @@ app.get("/posts/add", (req, res) => {
 })
 
 app.post("/posts/add",upload.single("featureImage"), (req, res) => {
-    let streamUpload = (req) => {
-        return new Promise((resolve, reject) => {
-            let stream = cloudinary.uploader.upload_stream(
-                (error, result) => {
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject(error);
-                }
-                }
-            );
-    
-            streamifier.createReadStream(req.file.buffer).pipe(stream);
-        });
-    };
-    
-    async function upload(req) {
-        let result = await streamUpload(req);
-        console.log(result);
-        return result;
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                    }
+                );
+        
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+        
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+        
+        upload(req).then((uploaded)=>{
+            req.body.featureImage = uploaded.url;
+            data.addPost(req.body).then(()=>{
+                res.redirect("/posts");
+            })
+        }); 
     }
-    
-    upload(req).then((uploaded)=>{
-        req.body.featureImage = uploaded.url;
-        data.addPost(req.body).then(()=>{
+    else{
+        processPost("");
+    }
+    function processPost(imageUrl){
+        req.body.featureImage = imageUrl;
+        blogData.addPost(req.body).then(post =>{
             res.redirect("/posts");
+        }).catch(err =>{
+            res.status(500).send(err);
         })
-    }); 
+    }
 })
 
 app.get("/post/:id", (req, res) => {
